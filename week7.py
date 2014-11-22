ee4688b171319805de26221061c375173260ca14
Regene <regenelee.miao@gmail.com>
2014-11-22 11:34:33 +0800

    week7

--- week7.py
+++ week7.py
@@ -1,8 +1,27 @@
+# Assignment 1
 # Use words.txt as the file name
 fname = raw_input("Enter file name: ")
 fh = open(fname)
 for line in fh:
-    #t = line.rstrip()
+    line = line.rstrip()
     t = line.upper()
     print t
 
+# Assignment 2
+# Use the file name mbox-short.txt as the file name
+fname = raw_input("Enter file name: ")
+fh = open(fname)
+count = 0
+summ = 0
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence: "):
+        count += 1
+        text1 = line
+        value = text1[-7:-1]
+        summ = summ + float(value)
+    else:
+        continue
+
+mean = summ/count
+
+print 'Average spam confidence:', mean
