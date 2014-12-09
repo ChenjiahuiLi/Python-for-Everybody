8e7f2b412828bcdae1dc2ee86283525d0436a358
Regene <regenelee.miao@gmail.com>
2014-12-09 10:45:50 +0800

    week9

--- week9_Q_A.py
+++ week9_Q_A.py
@@ -1,1 +1,22 @@
-__author__ = 'submarine'
+stuff = dict()
+print stuff.get('candy',-1)
+
+name = raw_input("Enter files:")
+if len(name) < 1: name = "mbox-short.txt"
+handle = open(name)
+
+email = list()
+for line in handle:
+    line = line.rstrip()
+    if not line.startswith('From'): continue
+    words = line.split()
+    t = words[1]
+    email.append(t)
+print len(email), email
+
+counts = dict()
+for name in email:
+    counts[name] = counts.get(name,0) + 1
+for key in counts:
+    if counts[key] == max(counts.values()):
+        print key, counts[key]
