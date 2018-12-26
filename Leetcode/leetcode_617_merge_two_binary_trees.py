f243d21f6eecb0a118dcdd8acdb62efd62d0a97d
Regene <regenelee.miao@gmail.com>
2018-12-25 22:11:20 -0800

    add test cases using binarytree pkg

--- Leetcode/leetcode_617_merge_two_binary_trees.py
+++ Leetcode/leetcode_617_merge_two_binary_trees.py
@@ -1,11 +1,5 @@
-import binarytree
-# Definition for a binary tree node.
-class Node:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
+from binarytree import tree, Node
+from random import seed
 
 class Solution:
     def mergeTrees(self, t1, t2):
@@ -14,16 +8,37 @@ class Solution:
         :type t2: Node
         :rtype: Node
         """
+        # Use t1 as the base tree for output
         if not t1 or not t2:
             return(t1 if not t2 else t2)
-        if t1 and t2:
-            t1.val = t1.val + t2.val
+        else:
+            t1.value += t2.value
             t1.left = self.mergeTrees(t1.left, t2.left)
             t1.right = self.mergeTrees(t1.right, t2.right)
         return(t1)
 
-tree1 = Node()
-tree2 = Node()
+
+
+seed(10086)      
+tree1 = tree(height = 3, is_perfect = True)
+tree2 = tree(height =2, is_perfect = False)
+print("Tree1")
+print(tree1)
+print("Tree2")
+print(tree2)
 s = Solution()
+
+# When one of the tree argument is empty
+stree1 = s.mergeTrees(tree1, None)
+print(stree1)
+
+# When both of the tree arguments are empty
+stree2 = s.mergeTrees(None, None)
+print(stree2)
+
+# When both of the tree arguments are non-empty
+stree3 = s.mergeTrees(tree1, tree2)
+print(stree3)
+
             
         
