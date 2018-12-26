430b7aabcbd5378a7fa2e2e506d231c911064493
Regene <regenelee.miao@gmail.com>
2018-12-25 21:55:46 -0800

    POC using binarytree pkg to generate trees for testing

---
+++ Leetcode/binary_tree.py
@@ -0,0 +1,19 @@
+from binarytree import Node, tree, bst, heap
+from random import seed
+
+# Build a tree with binarytree.tree
+seed(3)
+my_tree = tree(height = 3, is_perfect = False)
+print("Generate with built-in tree method")
+print(type(my_tree)) # <class 'binarytree.Node'>
+print(my_tree)
+
+
+# Build with Node
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+print("Generate with built-in Node method")
+print(root)
+
+
