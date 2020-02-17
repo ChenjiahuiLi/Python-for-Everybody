b1911eacd9ac09638378fd67cc8c94d0143b61ab
Regene <regenelee.miao@gmail.com>
2020-02-16 17:37:01 -0800

    Re-org code, add test_function() for Pytest testing

--- Leetcode/leetcode_14_longest_common_prefix.py
+++ Leetcode/leetcode_14_longest_common_prefix.py
@@ -1,64 +1,70 @@
+"""
+The gist:
+1. take out the i-th element of each string, if they are the same, i += 1
+2. how to make sure i does not goes too large? min(map(len,strs))
+
+To test with Pytest:
+1. add a test_function() in the code, use `assert` to build test cases
+2. ran `pytest leetcode_14_longest_common_predix.py` in command line
+
+Put the default example use case in the main() function
+"""
+
 class Solution:
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
         :rtype: str
         """
-        if not strs:
-        	return('')
-
-        result = []
-        total_iteration = min(map(len, strs))     
-        prefix_index = 0
-        start = 1
-
-        while start <= total_iteration:
-
-        	prefix_set = set()
-
-
-        	for string in strs:
-        		i_th_char = string[prefix_index]
-        		if i_th_char not in set():
-        			prefix_set.add(i_th_char)
-
-        	"""
-        	Compare: whether the ith prefix in each item is the same
-        	If all ith prefix are the same,
-        	- increment prefix_index by 1 for next iteration 
-        	- increment start by 1 to start the next iteration 
-
-        	If not,
-        	exit the While loop
-
-        	"""
-
-        	if len(prefix_set) == 1:
-        		result.append(prefix_set.pop())
-        		prefix_index += 1 
-        		start += 1
-        		continue
-        	else:
-        		break
-
-        return(''.join(result))
-
-
-
-def main():
+		# 1. setup the maxium length of the LCP would be the length of the shortest string 
+        # 2. for i = 0 to max_iteration - 1
+        #    take out the ith character of each of the string
+        #    instead of 1<>1 comparison, use a set() to store the ith char of each string
+        #    in the end of every i, count the number of items in the set() object
+        #    if equals 1, then it is a common prefix, added it to the returning object
+        #    else, break and return the returning object
+        if len(strs) < 1:
+            return ""
+        
+		# map(function, list): equals to apply function to each element of the list, return a list
+        max_iteration = min(map(len, strs))
+        lcp = [] # longest common prefix
+        start = 0
+        
+        while start < max_iteration:
+            common_char = set([char[start] for char in strs])
+            if len(common_char) == 1:
+                lcp.append(common_char.pop()) # set.pop() remove an random element from that set and return the poped element
+                start += 1
+            else:
+                break
+                
+        return ''.join(lcp)
+                
+                
+                
+def test_function():
 	test1 = Solution()
 	str1 = ['frequ','freq','fr']
-	print(test1.longestCommonPrefix(str1))
+	assert test1.longestCommonPrefix(str1) == 'fr'
 	str2 = ['frequ','freq','xr']
-	print(test1.longestCommonPrefix(str2))
+	assert test1.longestCommonPrefix(str2) == ""
 	str3 = ['frequ','freq','frequency']
-	print(test1.longestCommonPrefix(str3))
-	str4 = []
-	print(test1.longestCommonPrefix(str4))
-	str5 = ['','']
-	print(test1.longestCommonPrefix(str5))
+	assert test1.longestCommonPrefix(str3) == "freq"
+	str4 = [] # should give ""
+	assert test1.longestCommonPrefix(str4) == ""
+	str5 = ["a"] # should give "a"
+	assert test1.longestCommonPrefix(str5) == "a"
+	str6 = ["anna","anne","ab"] # should give "a"
+	assert test1.longestCommonPrefix(str6) == "ab"
 
-	
+def main():
+	test2 = Solution()
+	str_list = ["anna","anne","ana"]
+	print("Example string list is:")
+	print(str(str_list))
+	print("longest common prefix is:")
+	print(test2.longestCommonPrefix(str_list))	
 
 if __name__== "__main__":
 	main()
